e5f1864b1e40b594eb50d89f3ac7d0c25e10b575
ShaMan123 <shacharnen@gmail.com>
2020-12-20 18:37:44 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,26 @@ type ExcludeField<A, KeyToExclude extends keyof A> = { [K in ExcludeKey<keyof A,
 export declare type MathViewProps = PropsWithChildren<React.HTMLAttributes<MathfieldElement> & Partial<ExcludeField<MathfieldConfig, 'virtualKeyboardToggleGlyph'> & { value: string, virtualKeyboardToggleGlyph: JSX.Element }>>;
 export declare type MathViewRef = MathfieldElement;
 
+declare global {
+  /** @internal */
+  namespace JSX {
+    interface IntrinsicElements {
+      'math-field': MathfieldElement
+    }
+  }
+}
+
 const MathView = React.forwardRef<MathfieldElement, MathViewProps>((props, ref) => {
   const [customElementProps, _ref] = useCustomElement<MathfieldElement>(props);
   useImperativeHandle(ref, () => _ref.current!, [_ref]);
-  //@ts-ignore
-  return <math-field {...customElementProps} virtualKeyboardToggleGlyph={renderToString(props.virtualKeyboardToggleGlyph)} ref={_ref}>{props.children ? renderToString(props.children) : props.value}</math-field>
+  return (
+    <math-field
+      {...customElementProps}
+      ref={_ref}
+      //@ts-ignore
+      children={props.children ? renderToString(props.children as React.ReactElement) : props.value}
+    />
+  );
 });
 
 export default MathView;
