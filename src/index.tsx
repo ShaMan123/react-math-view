5b1c5eaf6c917dd1a723decb8136e76c6b8ca684
ShaMan123 <shacharnen@gmail.com>
2020-12-26 01:29:10 +0200

    fix wrong event name, remove hacks

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
-import { Mathfield, MathfieldElement } from 'mathlive';
+import { MathfieldElement } from 'mathlive';
 import 'mathlive/dist/mathlive-fonts.css';
 import 'mathlive/dist/mathlive.min';
-import React, { useCallback, useEffect, useImperativeHandle, useMemo, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useImperativeHandle, useMemo, useRef } from 'react';
 import { renderToString } from 'react-dom/server';
 import { MathViewProps } from './types';
-import { filterConfig, useAddChild, useEventDispatchRef, useUpdateOptions } from './utils';
+import { filterConfig, useEventRegistration, useUpdateOptions } from './utils';
 
 const MathView = React.forwardRef<MathfieldElement, MathViewProps>((props, ref) => {
   const _ref = useRef<MathfieldElement>(null);
   useImperativeHandle(ref, () => _ref.current!, [_ref]);
-
   const value = useMemo(() =>
     props.children ?
       renderToString(props.children as React.ReactElement)! :
@@ -18,6 +16,7 @@ const MathView = React.forwardRef<MathfieldElement, MathViewProps>((props, ref) 
     [props.children, props.value]
   );
   const [config, passProps] = useMemo(() => filterConfig(props), [props]);
+  useEventRegistration(_ref, props);
   useUpdateOptions(_ref, config);
   useEffect(() => {
     _ref.current?.setValue(value);
@@ -31,33 +30,10 @@ const MathView = React.forwardRef<MathfieldElement, MathViewProps>((props, ref) 
       onChange={undefined}
       ref={_ref}
     >
+      {value}
     </math-field>
   );
 });
 
-/**
- * This Component uses <input> and {useEventDispatchRef} as a workaround for bubbling change events to react
- * Motivation: 'onChange' is a must have in react, it is the basics of state handling
- * It fires an event from {onContentDidChange}
- */
-const MathViewWrapper = React.forwardRef<MathfieldElement, MathViewProps>((props, ref) => {
-  const [_input, dispatchEvent] = useEventDispatchRef();
-  const onContentDidChange = useCallback((sender: Mathfield) => {
-    props.onContentDidChange && props.onContentDidChange(sender);
-    dispatchEvent('change', { value: sender.getValue(), mathfield: sender });
-  }, [props.onContentDidChange]);
-
-  return (
-    <React.Fragment>
-      {ReactDOM.createPortal(<input hidden ref={_input} onChange={props.onChange as any} />, useAddChild('span'))}
-      <MathView
-        {...props}
-        ref={ref}
-        onContentDidChange={onContentDidChange}
-      />
-    </React.Fragment>
-  );
-})
-
 export * from './types';
-export default MathViewWrapper;
+export default MathView;
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,10 +1,10 @@
 import isEqual from 'lodash.isequal';
 import { MathfieldConfig } from "mathlive";
-import React, { useCallback, useLayoutEffect, useMemo, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import { renderToString } from "react-dom/server";
 import { MathViewProps, MathViewRef } from "./types";
 
-export const options: Array<keyof MathfieldConfig> = [
+export const OPTIONS: Array<keyof MathfieldConfig> = [
   "createHTML",
   "customVirtualKeyboardLayers",
   "customVirtualKeyboards",
@@ -62,11 +62,38 @@ export const options: Array<keyof MathfieldConfig> = [
   "virtualKeyboards",
 ];
 
+/**
+ * mount/unmount are unhandled
+ */
+const FUNCTION_MAPPING = {
+  /**retargeting onChange to fire input events to match react expected behavior */
+  onChange: 'input',
+  onInput: 'input',
+  /**rename onFocus to prevent name collision */
+  onMathFieldFocus: 'focus',
+  /**rename onBlur to prevent name collision */
+  onMathFieldBlur: 'blur',
+  onCommit: 'change',
+  //onContentDidChange,
+  //onContentWillChange,
+  onError: 'math-error',
+  onKeystroke: 'keystroke',
+  onModeChange: 'mode-change',
+  onMoveOutOf: 'focus-out',
+  onReadAloudStatus: 'read-aloud-status',
+  //onSelectionDidChange: 'selection-did-change',
+  onSelectionWillChange: 'selection-will-change',
+  //onTabOutOf,
+  onUndoStateDidChange: 'undo-state-did-change',
+  onUndoStateWillChange: 'undo-state-will-change',
+  onVirtualKeyboardToggle: 'virtual-keyboard-toggle',
+};
+
+const FUNCTION_PROPS = Object.keys(FUNCTION_MAPPING);
+
 const MAPPING = {
   className: 'class',
   htmlFor: 'for',
-  onMathFieldFocus: 'onFocus',
-  onMathFieldBlur: 'onBlur'
 };
 
 export function filterConfig(props: MathViewProps) {
@@ -75,7 +102,9 @@ export function filterConfig(props: MathViewProps) {
   for (const _key in props) {
     const key = MAPPING[_key] || _key;
     let value = props[_key];
-    if (options.indexOf(key) > -1) {
+    if (FUNCTION_PROPS.indexOf(key) > -1) {
+      //  handled by attaching event listeners
+    } else if (OPTIONS.indexOf(key) > -1) {
       if (React.isValidElement(value) || (value instanceof Array && value.every(React.isValidElement))) {
         value = renderToString(value as React.ReactElement);
       }
@@ -88,11 +117,12 @@ export function filterConfig(props: MathViewProps) {
 }
 
 /**
- * This hook is used to prevent the loss of caret position upon rendering (setting options causes what seems to be a full reset of Mathfield).
- * When an update was passed to the component the effect deemed the new config dep !== previous config dep, hence invoking `setOptions` and losing caret.
- * This solution will update options only if they have changed is comparison to the previous values (not object containing them), avoiding uncessary rendering.
- * 
- * !! The issue of losing caret when using `setOptions` consists. !!
+ * Performance Optimization
+ * ------------------------
+ * This hook memoizes config in order to prevent unnecessary rendering/changes
+ * The hook deemed the new config dep !== previous config dep, hence invoking `setOptions`.
+ * This solution will update options only if they have changed is comparison to the previous values (not object containing them),
+ *  avoiding uncessary rendering.
  * 
  * @param ref 
  * @param config 
@@ -105,63 +135,33 @@ export function useUpdateOptions(ref: React.RefObject<MathViewRef>, config: Part
       configRef.current = config;
     }
   }, [ref, config, configRef]);
+  // set options after rendering for first rendering pass, by then the mathfield has mounted and is able to receive it, before it mounted nothing happens
+  useEffect(() => {
+    ref.current?.setOptions(config);
+  }, []);
 }
 
-/**
- * @deprecated Events don't fire for some reason. Probably becasue mathfield resides in a shadow node.
- * https://github.com/facebook/react/issues/7901
- * @param node 
- * @param props 
- */
-export function handleEventRegistration(node: HTMLElement, props: MathViewProps) {
-  const fns: { key: string, fn: (customEvent: any) => any }[] = Object.keys(props)
-    .filter(key => typeof props[key] === 'function' && key.startsWith('on'))
-    .map(key => ({
-      key: MAPPING[key] || key,
-      fn: (...args: any[]) => { props[key](...args) },
-    }));
+export function useEventRegistration(ref: React.RefObject<HTMLElement>, props: MathViewProps) {
+  useEffect(() => {
+    const node = ref.current;
+    if (!node) return
+    const fns: { key: string, fn: (customEvent: any) => any }[] = Object.keys(props)
+      .filter(key => typeof props[key] === 'function' && FUNCTION_PROPS.indexOf(MAPPING[key] || key) > -1)
+      .map(key => {
+        return {
+          key: FUNCTION_MAPPING[MAPPING[key] || key],
+          fn: (...args: any[]) => { props[key](...args) },
+        }
+      });
 
-  fns.forEach(({ key, fn }) => {
-    node.addEventListener(key, fn);
-  });
+    fns.forEach(({ key, fn }) => {
+      node.addEventListener(key, fn);
+    });
 
-  return () => {
-    fns.forEach(({ key, fn }) =>
-      node.removeEventListener(key, fn),
-    );
-  };
-}
-
-/**
- * This hook enables change events to propagate to react
- * https://stackoverflow.com/questions/23892547/what-is-the-best-way-to-trigger-onchange-event-in-react-js#46012210
- * https://github.com/facebook/react/issues/11488
- * https://stackoverflow.com/a/62111884/9068029
- */
-export function useEventDispatchRef() {
-  const ref = useRef<HTMLInputElement>(null);
-  const dispatchEvent = useCallback((type: string, detail: { value: string, [key: string]: any }) => {
-    const handler = ref.current;
-    if (!handler) return;
-    const valueSetter = Object.getOwnPropertyDescriptor(handler, 'value')!.set!;
-    const prototype = Object.getPrototypeOf(handler);
-    const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value')!.set!;
-    if (valueSetter && valueSetter !== prototypeValueSetter) {
-      prototypeValueSetter.call(handler, detail.value);
-    } else {
-      valueSetter.call(handler, detail.value);
-    }
-    handler.dispatchEvent(new CustomEvent(type, { bubbles: true, cancelable: true, detail }));
-  }, [ref]);
-
-  return [ref, dispatchEvent] as [typeof ref, typeof dispatchEvent];
-}
-
-export function useAddChild(tagName: keyof HTMLElementTagNameMap) {
-  const container = useMemo(() => document.createElement(tagName), []);
-  useLayoutEffect(() => {
-    document.body.appendChild(container);
-    return () => { document.body.removeChild(container) };
-  }, [container]);
-  return container;
+    return () => {
+      fns.forEach(({ key, fn }) =>
+        node.removeEventListener(key, fn),
+      );
+    };
+  }, [ref, props])
 }
