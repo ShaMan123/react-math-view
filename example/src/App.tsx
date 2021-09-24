79c905561350b39acafd9a6a2214daf920d9df4f
ShaMan123 <shacharnen@gmail.com>
2021-09-24 15:51:41 +0300

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import MathView, { MathViewProps, MathViewRef } from 'react-math-view';
 
-const MathWithKeyboard = (props: MathViewProps) => {
+const MathWithKeyboard = React.memo((props: MathViewProps) => {
   const ref = useRef<MathViewRef>(null);
   return <MathView
     ref={ref}
@@ -16,9 +16,9 @@ const MathWithKeyboard = (props: MathViewProps) => {
     onChange={e => console.log('onChange', e.nativeEvent)}
     {...props}
   />
-}
+})
 
-const MathWithKeyboardButton = (props: MathViewProps) => {
+const MathWithKeyboardButton = React.memo((props: MathViewProps) => {
   const ref = useRef<MathViewRef>(null);
   return (
     <div className="inline">
@@ -35,9 +35,9 @@ const MathWithKeyboardButton = (props: MathViewProps) => {
       <span onClick={() => ref.current?.executeCommand('toggleVirtualKeyboard')} style={{ width: '21px', marginTop: '4px' }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M528 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h480c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm16 336c0 8.823-7.177 16-16 16H48c-8.823 0-16-7.177-16-16V112c0-8.823 7.177-16 16-16h480c8.823 0 16 7.177 16 16v288zM168 268v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm-336 80v-24c0-6.627-5.373-12-12-12H84c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm384 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zM120 188v-24c0-6.627-5.373-12-12-12H84c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm96 0v-24c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v24c0 6.627 5.373 12 12 12h24c6.627 0 12-5.373 12-12zm-96 152v-8c0-6.627-5.373-12-12-12H180c-6.627 0-12 5.373-12 12v8c0 6.627 5.373 12 12 12h216c6.627 0 12-5.373 12-12z"></path></svg></span>
     </div>
   );
-}
+})
 
-const ControlledMathView = (props: MathViewProps) => {
+const ControlledMathView = React.memo((props: MathViewProps) => {
   const [value, setValue] = useState(props.value || "$$d=\\sqrt[]{x^2+y^2}$$");
   const onChange = useCallback((e: React.SyntheticEvent<any, any>) => {
     setValue(e.currentTarget.getValue());
@@ -49,17 +49,15 @@ const ControlledMathView = (props: MathViewProps) => {
     console.log('ControlledMathView value changed', value);
   }, [value]);
 
-
-
   return (
     <MathView
       {...props}
       ref={ref}
-      value={value}
       onChange={onChange}
+      value={value}
     />
   )
-}
+})
 
 const WithSetOptions = (props: MathViewProps) => {
   const ref = useRef<MathViewRef>(null);
@@ -77,7 +75,7 @@ const App = () => {
     setTimeout(() => {
       setValue("a^2+b^2=c^2");
       setK("onfocus");
-    }, 1500);
+    }, 3500);
   })
   const [enabled, setEnabled] = useState(true);
   return (
@@ -146,7 +144,7 @@ const App = () => {
       <MathView virtualKeyboardMode={k} className="red">{value}</MathView>
       <p>
         <MathWithKeyboard style={{ backgroundColor: 'blueviolet' }} value="\gamma" />
-        <MathWithKeyboard ignoreSpacebarInMathMode={false} value="\delta" />
+        <MathWithKeyboard value="\delta" />
       </p>
       <MathWithKeyboardButton>{"x=\\frac{-b\\pm\\sqrt{b ^ 2 - 4ac}}{2a}"}</MathWithKeyboardButton>
       <div style={{ margin: 50 }}>
